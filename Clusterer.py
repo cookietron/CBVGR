626fcfe5262c46b42aba9732e48d32a99b44806e
Stephen <cookietron@gmail.com>
2014-11-19 18:44:33 -0800

    Create Clusterer.py

---
+++ Clusterer.py
@@ -0,0 +1,14 @@
+# define a class for the individual logs
+class GameLog():
+  pass
+# define a class for a similarity distance between two logs
+class Distance():
+  def __init__(self, Log1, Log2):
+  # set Distance properties here
+    pass
+# create a list of game logs?
+game_index = []
+# create a second list for distances?
+distance_index = []
+# use os.walk to look through the folder of logs and create 
+
